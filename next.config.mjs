a590a654fdedc4ed0fd83fee581f6f9ae949bf17
ghostGodKing <ghostgodlessking@gmail.com>
2025-07-06 16:59:39 +0530

    Initial commit2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default nextConfig = {
   async rewrites() {
     return [
       {
@@ -9,5 +9,3 @@ const nextConfig = {
     ]
   },
 }
-
-export default nextConfig
